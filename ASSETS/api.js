43ff39444393d4c04bf82786733c30b73e6b4fb3
Soumilgit <soumil.m@somaiya.edu>
2025-08-30 18:29:04 +0530

    Feat: Fixed blank div return for username search, reduced code redundancy

--- ASSETS/api.js
+++ ASSETS/api.js
@@ -4,31 +4,41 @@ submit.addEventListener("click", (e) => {
 })
 
 const showGithub = (username) => {
+  username = username.split(' ').join('');
 
-    document.getElementById("content").style.display = "flex";
+  if (username === "") {
+    window.alert("Please Enter your UserName to search in Github");
+    return;
+  }
 
-    username = username.split(' ').join('');
+  document.getElementById("content").style.display = "flex";
 
-    fetch("https://api.github.com/users/" + username)
-        .then(response => response.json())
-        .then(response => {
-            if (username === "") {
-                window.alert("Please Enter your UserName to search in Github")
-            } else {
+  fetch(`https://api.github.com/users/${username}`)
+    .then(response => response.json())
+    .then(response => {
+      if (response.message === "Not Found") {
+        window.alert("Username not found. Please enter a valid Github account.");
+        document.getElementById("content").style.display = "none";
+        return;
+      }
 
-                if (response.name === undefined) {
-                    window.alert("Username Not found. Please create your Github account.")
-                }
-                else {
-                    img.innerHTML = `<a target="_blank" href="https://github.com/${username}"> <img src="${response.avatar_url}"/></a>`
-                    uname.innerHTML = response.name
-                
-                    followers.innerHTML = `<strong> Followers </strong> : ${response.followers}`
-                    following.innerHTML = `<strong> Following </strong> : ${response.following}`
-                    
-                    repos.innerHTML = `<strong> Total Projects </strong> : ${response.public_repos}`
-                }
-            }
-        })
-        .catch(err => console.error(err));
-}
+      uname.innerHTML = response.name || response.login;
+      img.innerHTML = `
+        <a target="_blank" href="https://github.com/${username}">
+          <img src="${response.avatar_url}" alt="${username}'s avatar"/>
+        </a>`;
+
+      bio.innerHTML = response.bio ? `<strong>Bio:</strong> ${response.bio}` : "";
+      joined.innerHTML = `<strong>Joined:</strong> ${new Date(response.created_at).toDateString()}`;
+      followers.innerHTML = `<strong>Followers:</strong> ${response.followers}`;
+      following.innerHTML = `<strong>Following:</strong> ${response.following}`;
+      repos.innerHTML = `<strong>Repos:</strong> ${response.public_repos}`;
+    })
+    .catch(err => {
+      console.error(err);
+      window.alert("An error occurred while fetching data.");
+    });
+};
+
+
+
